7d484a0a0d6892d892bcc65c258cdf6894940d76
Xechronos <abechdol@student.neumont.edu>
2025-03-04 09:10:35 -0700

    Schema Updated

--- back-end/classes/user.js
+++ back-end/classes/user.js
@@ -1,10 +1,42 @@
+<<<<<<< Updated upstream
 export default class user{
 constructor(email, username, password){
+=======
+const { default: mongoose } = require("mongoose");
+
+class User {
+  constructor(email, userId, username, password, profilePicture, bio, creationDate, uploadedVidsIDList, uploadedCommentsIDList) {
+>>>>>>> Stashed changes
     this.email = email;
     this.username = username;
     this.password = password;
 }
+<<<<<<< Updated upstream
 toString() {
     return `User: { email: ${this.email}, username: ${this.username}, password: ${this.password} }`;
   }
 }
+=======
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: string,
+    index: { unique: true }
+  },
+  username: string,
+  password: string,
+  profilePicture: string,
+  userId: {
+    type: int,
+    index: {unique: true}
+  },
+  bio: string,
+  creationDate: string,
+  uploadedVidsIDList: string,
+  uploadedCommentsIDList: string
+
+
+})
+
+module.exports = mongoose.model(User, UserSchema);
+>>>>>>> Stashed changes
--- back-end/classes/video.js
+++ back-end/classes/video.js
@@ -1,12 +1,46 @@
+<<<<<<< Updated upstream
 export default class video{
     constructor( title, postDate, googleUploadLink, uploaderID){
         this.title = title;
         this.postDate = postDate;
         this.googleUploadLink = googleUploadLink;
         this.uploaderID = uploaderID;
+=======
+const { default: mongoose } = require("mongoose");
+
+class Video {
+    constructor(videoID, title, postDate, googleDriveLink, userID) {
+      this.videoID = videoID;
+      this.title = title;
+      this.postDate = postDate;
+      this.googleDriveLink = googleDriveLink;
+      this.userID = userID;
+>>>>>>> Stashed changes
     }
 
     toString() {
+<<<<<<< Updated upstream
         return `Video: { title: ${this.title}, postDate: ${this.postDate}, googleUploadLink: ${this.googleUploadLink}, uploaderID: ${this.uploaderID} }`;
       }
 }
+=======
+      return `Video: { Title: ${this.title}, Post Date: ${this.postDate}, Google Upload Link: ${this.googleDriveLink}, UploaderID: ${this.userID}}`;
+    }
+  }
+  
+  const VideoSchema = new mongoose.Schema({
+    videoId: {
+      type: int,
+      index: { unique: true }
+    },
+    title: string,
+    postDate: string,
+    googleDriveLink: string,
+    userId: {
+      type: int,
+      index: {unique: true}
+    }
+  })
+
+  module.exports = mongoose.model(Video, VideoSchema);
+>>>>>>> Stashed changes
