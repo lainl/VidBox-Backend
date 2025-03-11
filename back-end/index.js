f8811a01687cc95ef0f7e1f94f6fa9048c5131ed
Xechronos <abechdol@student.neumont.edu>
2025-03-11 10:04:50 -0600

    Classes Updated

--- back-end/index.js
+++ back-end/index.js
@@ -1,33 +1,29 @@
 const express = require("express");
-
-const PORT = 3000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+const PORT = 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(cors());
 
-app.get("/", (req, res) => {
-    console.log("/ hit");
 
-    var responseJson = {
-        message: "Welcome to VidBox",
-        routes: [
-            {
-                route: "/",
-                details: "Home route"
-            },
-            {
-                route: "/login",
-                details: "prompt login/signup"
-            }
-        ]
-    };
+app.use(require("./Routes/home"));
+app.use(require("./Routes/video"));
+app.use(require("./Routes/user"));
+app.use(require("./Routes/auth"));
+app.use(require("./Routes/profile"));
 
-    res.json(responseJson);
-})
 
-app.listen(PORT, (err) => {
+if (require.main === module) {
+  app.listen(PORT, (err) => {
     if (err) console.log(err);
     console.log(`Express Listening http://localhost:${PORT}/`);
-})
+  });
+}
+
+module.exports = app;
